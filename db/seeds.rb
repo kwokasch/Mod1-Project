3ea7c0986b9a70e20aa80e358150f949d3a85b91
github username <github email address>
2019-09-12 11:47:39 -0600

    refactored CLI methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
-# MoodEvent.destroy_all
-# Mood.destroy_all
-# Event.destroy_all
+MoodEvent.destroy_all
+Mood.destroy_all
+Event.destroy_all
 
 
 pumped = Mood.create(name: "PUMPED!")
 
 fizzfest = Event.create({
-    name: "NAME: Fizz Fight - A Hard Seltzer Festival",
-    description: "DESCRIPTION: Enjoy an afternoon sampling a variety of hard seltzers from companies across the country!",
-    date: "DATE: Saturday, September 14, 2019",
-    time: "TIME: 11:30AM - 7:30PM",
-    location: "LOCATION: EXDO, 1399 35th Street, Denver, CO 80205"
+    name: "Fizz Fight - A Hard Seltzer Festival",
+    description: "Enjoy an afternoon sampling a variety of hard seltzers from companies across the country!",
+    date: "Saturday, September 14, 2019",
+    time: "11:30AM - 7:30PM",
+    location: "EXDO, 1399 35th Street, Denver, CO 80205"
 })
 
 dbqueen = Event.create({
-    name: "NAME: David Bowie X Queen",
-    description: "DESCRIPTION: A Special DJ tribute to the greatest of our time. Get ready for a dance party like no other!",
-    date: "DATE: Friday, September 13, 2019",
-    time: "TIME: 7:00PM - 11:59PM",
-    location: "LOCATION: The Monkey Barrel, 4401 Tejon Street, Denver, CO 80211"
+    name: "David Bowie X Queen",
+    description: "A Special DJ tribute to the greatest of our time. Get ready for a dance party like no other!",
+    date: "Friday, September 13, 2019",
+    time: "7:00PM - 11:59PM",
+    location: "The Monkey Barrel, 4401 Tejon Street, Denver, CO 80211"
 })
 
 p1 = MoodEvent.create({
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,274 +1,62 @@
 
 require 'tty-prompt'
+require 'colorize'
 
 class Cli
     @@prompt = TTY::Prompt.new
 
-    puts "Hello! Welcome to Mood Events!"
+    def main_menu
+        puts "Hello! Welcome to Mood Events!"
+        puts "Please select your mood."
 
-    def mood_menu
-        puts "Let's find something to do based on your mood!"
-        mood_choice = @@prompt.select("How are you feeling today?", ["PUMPED!", "Active", "Social", "Chill", "Introverted", "Stressed", "Not sure, surprise me!"])
-   
-        if mood_choice == "PUMPED!"
-            system("clear")
-            puts "Let's get pumped up!" 
-            event_choice = @@prompt.select("Choose one:", ["Fizz Fight - A Hard Seltzer Festival", "David Bowie X Queen", "EVENT 3", "Pick a different mood"])
-            system("clear")
+        mood_choice = @@prompt.select("How are you feeling today?", Mood.all.map(&:name)) 
+            # Mood.all.map do |mood| 
+            #     case mood.name 
+            #     when "PUMPED!" 
+            #         mood.name.red
+            #     when "PUMPED!" 
+            #         mood.name.light_red
+            #     when "PUMPED!" 
+            #         mood.name.yellow
+            #     when "PUMPED!" 
+            #         mood.name.light_cyan
+            #     when "PUMPED!" 
+            #         mood.name.cyan
+            #     when "PUMPED!" 
+            #         mood.name.blue
+            #     when "PUMPED!" 
+            #         mood.name.light_magenta
+            #     end
+            # end)
+        system("clear")
+        @mood = Mood.find_by(name: mood_choice)
+        # binding.pry
+        found_events
+        puts "Here are the events for your mood! Please select one:"
+        puts " "
+        event_choice = @@prompt.select("Events:", found_events.pluck(:name))
+        @event = Event.find_by(name: event_choice)
+        event_info
+        # binding.pry
+    end
 
-            if event_choice == "Fizz Fight - A Hard Seltzer Festival"
-                puts "Here's your event info!:"
-                puts " "
+    def found_events
+        @mood.events
+    end
 
-                found_event = Mood.find_by(name: "PUMPED!").events[0]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "David Bowie X Queen"
-                puts "Here's your event info!:"
-                puts " "
-                
-                found_event = Mood.find_by(name: "PUMPED!").events[1]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 3"
-                puts "Here's your event info!:"
-                puts " "
-                
-                found_event = Mood.find_by(name: "PUMPED!").events[2]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "Pick a different mood"
-                mood_menu
-            end
-        elsif mood_choice == "Active"
-            system("clear")
-            puts "Let's get moving!" 
-            event_choice = @@prompt.select("Choose one:", ["EVENT 1", "EVENT 2", "EVENT 3", "Pick a different mood"])
-        
-            if event_choice == "EVENT 1"
-                puts "Here's your event info!:"
-                puts " "
-                
-                found_event = Mood.find_by(name: "Active").events[0]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 2"
-                puts "Here's your event info!:"
-                puts " "
-                
-                found_event = Mood.find_by(name: "Active").events[1]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 3"
-                puts "Here's your event info!:"
-                puts " "
-                
-                found_event = Mood.find_by(name: "Active").events[2]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "Pick a different mood"
-                mood_menu
-            end
-        elsif mood_choice == "Social"
-            system("clear")
-            puts "Let's get social!" 
-            event_choice = @@prompt.select("Choose one:", ["EVENT 1", "EVENT 2", "EVENT 3", "Pick a different mood"])
-        
-            if event_choice == "EVENT 1"
-                puts "Here's your event info!:"
-                puts " "
-                
-                found_event = Mood.find_by(name: "Feeling social").events[0]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 2"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Feeling social").events[1]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 3"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Feeling social").events[2]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "Pick a different mood"
-                mood_menu
-            end
-        elsif mood_choice == "Chill"
-            system("clear")
-            puts "Let's be chill." 
-            event_choice = @@prompt.select("Choose one:", ["EVENT 1", "EVENT 2", "EVENT 3", "Pick a different mood"])
-        
-            if event_choice == "EVENT 1"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Chill").events[0]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 2"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Chill").events[1]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 3"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Chill").events[2]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "Pick a different mood"
-                mood_menu
-            end
-        elsif mood_choice == "Introverted"
-            system("clear")
-            puts "Let's have some alone time." 
-            event_choice = @@prompt.select("Choose one:", ["EVENT 1", "EVENT 2", "EVENT 3", "Pick a different mood"])
-        
-            if event_choice == "EVENT 1"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Introverted").events[0]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 2"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Introverted").events[1]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 3"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Introverted").events[2]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "Pick a different mood"
-                mood_menu
-            end
-        elsif mood_choice == "Stressed"
-            system("clear")
-            puts "Let's blow off some steam." 
-            event_choice = @@prompt.select("Choose one:", ["EVENT 1", "EVENT 2", "EVENT 3", "Pick a different mood"])
-        
-            if event_choice == "EVENT 1"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Stressed").events[0]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 2"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Stressed").events[1]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "EVENT 3"
-                puts "Here's your event info!:"
-                puts " "
-
-                found_event = Mood.find_by(name: "Stressed").events[2]
-                puts found_event.name
-                puts found_event.description
-                puts found_event.date
-                puts found_event.time
-                puts found_event.location
-                puts " "
-            elsif event_choice == "Pick a different mood"
-                mood_menu
-            end
-        elsif mood_choice == "Not sure, surprise me!"
-            system("clear")
-            puts "Here's your event info!:"
-            puts " "
-
-            random_event = Event.all.sample
-            puts random_event.name
-            puts random_event.description
-            puts random_event.date
-            puts random_event.time
-            puts random_event.location
-            puts " "
-        end
+    def event_info
+        puts " "
+        puts "DESCRIPTION:".cyan
+        puts "#{@event.description}"
+        puts " "
+        puts "DATE:".light_cyan
+        puts "#{@event.date}"
+        puts " "
+        puts "TIME:".light_blue
+        puts "#{@event.time}"
+        puts " "
+        puts "LOCATION:".light_magenta
+        puts "#{@event.location}"
+        puts " "
     end
 end
